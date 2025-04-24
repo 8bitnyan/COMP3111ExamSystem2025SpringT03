2b7165c0238cf3dd2c9b17519a430024c127fcc4
ggongtaek <ytoh9876@gmail.com>
2025-04-24 15:28:19 +0800

    Task Part 2

---
+++ src/main/java/comp3111/examsystem/entity/Exam.java
@@ -0,0 +1,63 @@
+package comp3111.examsystem.entity;
+
+import java.util.List;
+
+public class Exam extends Entity {
+    private String courseCode;
+    private String examName;
+    private long examTime;
+    private List<Long> questions;
+    private boolean isPublished;
+
+    public Exam() {}
+
+    public Exam(String courseCode, String examName, long examTime, List<Long> questions, boolean isPublished) {
+        this.courseCode = courseCode;
+        this.examName = examName;
+        this.examTime = examTime;
+        this.questions = questions;
+        this.isPublished = false;
+    }
+
+    //Getter Function
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getExamName() {
+        return examName;
+    }
+
+    public long getExamTime() {
+        return examTime;
+    }
+
+    public List<Long> getQuestions() {
+        return questions;
+    }
+
+    public boolean getIsPublished() {
+        return isPublished;
+    }
+
+    //Setter Function
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public void setExamName(String examName) {
+        this.courseCode = courseCode;
+    }
+
+    public void setExamTime(Long examTime) {
+        this.courseCode = courseCode;
+    }
+
+    public void setIsPublished(boolean isPublished) {
+        this.isPublished = isPublished;
+    }
+
+    public void setQuestions(List<Long> newQuestions) {
+        this.questions = newQuestions;
+    }
+}
---
+++ src/main/java/comp3111/examsystem/entity/Question.java
@@ -0,0 +1,108 @@
+package comp3111.examsystem.entity;
+
+import java.util.List;
+
+public class Question extends Entity {
+    private Long teacherId; // To isolate questions per teacher
+    private String type; // e.g., "MCQ", "Short Answer"
+    private String questionText;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private String answer;
+    private Integer score;
+    private Integer isPublished = 0; // For confirmation of deletion
+
+    // Constructors
+    public Question() {} // Required for Database.java reflection
+
+    public Question(Long teacherId, String type, String questionText, String A, String B, String C, String D, String answer, int score) {
+        this.teacherId = teacherId;
+        this.type = type;
+        this.questionText = questionText;
+        this.optionA = A;
+        this.optionB = B;
+        this.optionC = C;
+        this.optionD = D;
+        this.answer = answer;
+        this.score = score;
+    }
+
+    // Getters and Setters
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+    public String getOptionB() {
+        return optionB;
+    }
+    public String getOptionC() {
+        return optionC;
+    }
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public void setA(String A) {
+        this.optionA = A;
+    }
+    public void setB(String B) {
+        this.optionB = B;
+    }
+    public void setC(String C) {
+        this.optionC = C;
+    }
+    public void setD(String D) {
+        this.optionD = D;
+    }
+
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public int getPublished() {
+        return isPublished;
+    }
+
+    public void setPublished(int publish) {
+        isPublished = publish;
+    }
+}
